5b3fce630cae73d496f95491d4a13ef01a304dfa
Aishwarya Parthasarathy <aishu.parth@gmail.com>
2019-04-11 21:23:05 +0200

    bugs

--- BehavioralAnalysis.m
+++ BehavioralAnalysis.m
@@ -1,32 +1,44 @@
-function [go_correct,nogo_correct] =  BehavioralAnalysis(filename)
+function [go_reward,go_punishment] =  BehavioralAnalysis(filename)
 [A,D,E] =  ProcessMedPC(filename);
 n_trials = find(E==2);
 n_trialstart = find(E==1);
-go_correct = 0;go_incorrect = 0;nogo_correct=0;nogo_incorrect=0;
+go_reward = 0; go_punishment = 0;n_reward =0;n_punishment = 0;
+% go_correct = 0;go_incorrect = 0;nogo_correct=0;nogo_incorrect=0;go_punishcorr=0;nogo_punishcorr=0;go_punisherr=0;nogo_punisherr=0;
 for i = 1:length(n_trials)
     if i ==length(n_trials)
         tmp = E(n_trials(i):end);
     else
         tmp = E(n_trials(i):n_trialstart(i+1));
     end
-    if length(find(tmp==19)) + length(find(tmp==3))==2
-        go_correct = go_correct +1; 
-    elseif length(find(tmp==19)) + length(find(tmp==5))==2
-        go_incorrect = go_incorrect+1;
-    elseif length(find(tmp==19)) + length(find(tmp==17))==2
-        go_incorrect = go_incorrect+1;
-    elseif length(find(tmp==20 )) + length(find(tmp==3))==2
-        nogo_correct = nogo_correct+1;
-    elseif length(find(tmp==20))+length(find(tmp==21))==2
-         nogo_incorrect = nogo_incorrect+1;
-    end
-end
-n_corrtrials = length(find(E==3));
-n_corrgo = length(find(E==19));
-n_corrnogo = length(find(E==20));
-go_correct = go_correct/n_corrgo;
-nogo_correct = nogo_correct/n_corrnogo;
+        if  length(find(tmp==3))==1
+        go_reward = go_reward +1; 
+    elseif  length(find(tmp==4))==1
+        go_punishment = go_punishment+1;
+        elseif length(find(tmp==14))==1
+        end
+%     if length(find(tmp==19)) + length(find(tmp==3))==2
+%         go_correct = go_correct +1; 
+%     elseif length(find(tmp==19)) + length(find(tmp==5))==2
+%         go_incorrect = go_incorrect+1;
+%     elseif length(find(tmp==19)) + length(find(tmp==17))==2
+%         go_incorrect = go_incorrect+1;
+%     elseif length(find(tmp==20 )) + length(find(tmp==3))==2
+%         nogo_correct = nogo_correct+1;
+%     elseif length(find(tmp==20))+length(find(tmp==21))==2
+%          nogo_incorrect = nogo_incorrect+1;
+%      
+%     end
 
+end
+% n_corrtrials = length(find(E==3));
+% n_corrgo = length(find(E==19));
+% n_corrnogo = length(find(E==20));
+% go_correct = go_correct/n_corrgo;
+% nogo_correct = nogo_correct/n_corrnogo;
+n_reward = length(find(E==19));
+n_punishment = length(find(E==20));
+go_reward = go_reward/n_reward;
+go_punishment = go_punishment/n_punishment;
 
 end
 
